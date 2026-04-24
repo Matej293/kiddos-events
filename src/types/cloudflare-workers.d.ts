08919f94948861190163d489674ae4aa2bd40ce1
Matej293 <matejspajic16@gmail.com>
2026-04-24 16:37:17 +0200

    read site key from Workers   runtime env first

--- src/types/cloudflare-workers.d.ts
+++ src/types/cloudflare-workers.d.ts
@@ -2,6 +2,7 @@ declare module 'cloudflare:workers' {
   export const env: {
     RESEND_API_KEY?: string;
     TURNSTILE_SECRET_KEY?: string;
+    PUBLIC_TURNSTILE_SITE_KEY?: string;
     [key: string]: string | undefined;
   };
 }
